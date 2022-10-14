1bbd710f663c8b7e9aa2d7d84cf0652af6e6950c
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-10-14 19:23:02 +0530

    Update Line.jsx

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import {Header, Line } from '../../components';
+import {Header, LineChart } from '../../components';
 
 const Line = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
     <Header category="Line" title="Inflation Rate" />
     <div className="w-full">
-      <Line />
+      <LineChart />
     </div>
   </div>
 );
