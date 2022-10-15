7504da1501c8d01cad61d6b41402dcfa7fadb240
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-10-15 15:50:05 +0530

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { DialogComponent } from '@syncfusion/ej2-react-popups';
 
 const Notification = ()=>{
     return (
         <div>
-            Notification
+            <DialogComponent width='250px' target='#dialog-target' showCloseIcon={true} header='Dialog' closeOnEscape={false}>
+          This is a dialog with header </DialogComponent>
         </div>
     )
 }
